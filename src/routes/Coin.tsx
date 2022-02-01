766920baa43b1636ce14373eb80296cbcfdbac2f
jerome <jerome@geekstudio.co.kr>
2022-02-01 21:50:01 +0900

    feat: 코인 상세 페이지내 중첩 라우팅 추가

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,16 @@
 import { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import {
+    Link,
+    Route,
+    Switch,
+    useLocation,
+    useParams,
+    useRouteMatch,
+} from 'react-router-dom';
 import styled from 'styled-components';
+
+import Chart from 'routes/Chart';
+import Price from 'routes/Price';
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -18,6 +28,54 @@ const Header = styled.header`
 const Title = styled.h1`
     font-size: 48px;
     color: ${(props) => props.theme.accentColor};
+`;
+
+const Overview = styled.div`
+    display: flex;
+    justify-content: space-between;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 10px 20px;
+    border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    span:first-child {
+        font-size: 10px;
+        font-weight: 400;
+        text-transform: uppercase;
+        margin-bottom: 5px;
+    }
+`;
+
+const Description = styled.p`
+    margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin: 25px 0px;
+    gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: 400;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 7px 0px;
+    border-radius: 10px;
+    color: ${(props) =>
+        props.isActive ? props.theme.accentColor : props.theme.textColor};
+
+    a {
+        display: block;
+    }
 `;
 
 const Loader = styled.span`
@@ -157,6 +215,8 @@ const Coin = () => {
     const [priceInfo, setPriceInfo] = useState<PriceData>();
     const { coinId } = useParams<RouteParams>();
     const { state } = useLocation<RouteState>();
+    const priceMatch = useRouteMatch('/:coinId/price');
+    const chartMatch = useRouteMatch('/:coinId/chart');
 
     useEffect(() => {
         (async () => {
@@ -169,15 +229,67 @@ const Coin = () => {
 
             setInfo(infoData);
             setPriceInfo(priceData);
+            setLoading(false);
         })();
-    }, []);
+    }, [coinId]);
 
     return (
         <Container>
             <Header>
-                <Title>{state?.name || 'Loading...'}</Title>
+                <Title>
+                    {state?.name
+                        ? state.name
+                        : loading
+                        ? 'Loading...'
+                        : info?.name}{' '}
+                </Title>
             </Header>
-            {loading ? <Loader>Loading...</Loader> : null}
+
+            {loading ? (
+                <Loader>Loading...</Loader>
+            ) : (
+                <>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Rank</span>
+                            <span>{info?.rank}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Symbol</span>
+                            <span>{info?.symbol}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Description>{info?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Total Supply:</span>
+                            <span>{priceInfo?.total_supply}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Max Supply:</span>
+                            <span>{priceInfo?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
+
+                    <Tabs>
+                        <Tab isActive={chartMatch !== null}>
+                            <Link to={`/${coinId}/chart`}>Chart</Link>
+                        </Tab>
+                        <Tab isActive={priceMatch !== null}>
+                            <Link to={`/${coinId}/price`}>Price</Link>
+                        </Tab>
+                    </Tabs>
+
+                    <Switch>
+                        <Route path={`/${coinId}/price`}>
+                            <Price />
+                        </Route>
+                        <Route path={`/${coinId}/chart`}>
+                            <Chart />
+                        </Route>
+                    </Switch>
+                </>
+            )}
         </Container>
     );
 };
